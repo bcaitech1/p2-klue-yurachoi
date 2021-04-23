41fcff73c2595082fcf01346cc30dd6d84e25fdb
Yuuraa <yoorachoi8937@gmail.com>
2021-04-23 02:07:24 +0000

    Define new dataset with [REL] token & Add new load function

--- code/dataset.py
+++ code/dataset.py
@@ -5,24 +5,30 @@ import pickle
 from transformers import AutoTokenizer
 import torch
 
+from sklearn.model_selection import StratifiedKFold
+
+
 
 def init_tokenizer(pretrained_model, additional_tokens=[]):
-    tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
+    """
+    Function that init tokenizer, which loads proper tokenizer and add tokens in given list of tokens
+    - pretrained_model: Tokenizer pretrained model
+    - additional_tokens: Tokens to add to the tokenizer
+
+    - returns: tokenizer & number of additional tokens
+    """
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
     if additional_tokens:
         tokenizer.add_special_tokens({'additional_special_tokens': additional_tokens})
-    return tokenizer
-
-
-def build_train_data(tokenizer, data_dir):
-    train_df = pd.read_csv(data_dir, delimiter="\t", names=["documentID", "text", "ent1", "ent1_start", "ent1_end", "ent2", "ent2_start", "ent2_end", "label"])
+    return tokenizer, len(additional_tokens)
 
 
 # bert input을 위한 tokenizing.
 # tip! 다양한 종류의 tokenizer와 special token들을 활용하는 것으로도 새로운 시도를 해볼 수 있습니다.
 # baseline code에서는 2가지 부분을 활용했습니다.
-def tokenized_dataset(dataset, tokenizer):
+def tokenize_dataset(dataset, tokenizer):
     concat_entity = []
-    for e01, e02 in zip(dataset['ent1'], dataset['ent2']):
+    for e01, e02 in zip(dataset['entity1'], dataset['entity2']):
         temp = ''
         temp = e01 + '[SEP]' + e02
         concat_entity.append(temp)
@@ -38,15 +44,18 @@ def tokenized_dataset(dataset, tokenizer):
     return tokenized_sentences
 
 
+
+
 def tokenize_rel_dataset(dataset, tokenizer):
     rel_masked_sentences = []
     char1_indices = []
     char2_indices = []
     rel1_indices = [] # 토큰화 되었을 때
     rel2_indices = [] # 토큰화 되었을 때
-    for text, beg1, end1, beg2, end2 in zip(dataset['text'], dataset['ent1_start'], dataset['ent1_end'], dataset['ent2_start'], dataset['ent2_end']):
-        text, token1_loc, token2_loc = mask_special_token(text, beg1, end1, beg2, end2)
-        rel_masked_sentences.append(text)
+    for text, beg1, end1, beg2, end2 in zip(dataset['text'], dataset['beg1'], dataset['end1'], dataset['beg2'], dataset['end2']):
+        masked_text, token1_loc, token2_loc = mask_special_token(text, beg1, end1, beg2, end2)
+        # tokenize_check(text, beg1, end1, beg2, end2, tokenizer)
+        rel_masked_sentences.append(masked_text)
         char1_indices.append(token1_loc)
         char2_indices.append(token2_loc)
     
@@ -80,25 +89,7 @@ class RE_Dataset(torch.utils.data.Dataset):
         return len(self.labels)
 
 
-def load_pd_data(data_dir):
-    """
-    학습 데이터를 pandas DataFrmae 형태로 읽어들이고, label을 id로 변환한다
-    - data_dir: train tsv 파일의 경로
-    """
-    with open('/opt/ml/input/data/label_type.pkl', 'rb') as f:
-        label_type = pickle.load(f)
-    # load dataset
-    dataset = pd.read_csv(data_dir, delimiter='\t', names=["documentID", "text", "ent1", "ent1_start", "ent1_end", "ent2", "ent2_start", "ent2_end", "label"])
-    label = []
-    for i in dataset['label']:
-        if i == 'blind':
-            label.append(100)
-        else:
-            label.append(label_type[i])
-    dataset['label'] = label
-    return dataset
-
-
+# Semantic relationship task를 위한 데이터셋
 class RelationDataset(torch.utils.data.Dataset):
     def __init__(self, tokenized_dataset, ent1_locs, ent2_locs, labels):
         self.tokenized_dataset = tokenized_dataset
@@ -115,6 +106,53 @@ class RelationDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.labels)
+
+
+def load_test_dataset(dataset_dir, tokenizer):
+    test_dataset = load_pd_data(dataset_dir)
+    test_label = test_dataset['label'].values
+    # tokenizing dataset
+    tokenized_test = tokenize_dataset(test_dataset, tokenizer)
+    return tokenized_test, test_label
+
+
+def load_semantic_testset(dataset_dir, tokenizer):
+    test_dataset = load_pd_data(dataset_dir)
+    test_label = test_dataset['label'].values
+    # tokenizing dataset
+    tokenized_test, ent1_locs, ent2_locs = tokenize_rel_dataset(test_dataset, tokenizer)
+    return tokenized_test, ent1_locs, ent2_locs, test_label
+
+
+
+def load_pd_data(data_dir):
+    """
+    학습 데이터를 pandas DataFrmae 형태로 읽어들이고, label을 id로 변환한다
+    - data_dir: train tsv 파일의 경로
+    """
+    with open('/opt/ml/input/data/label_type.pkl', 'rb') as f:
+        label_type = pickle.load(f)
+    # load dataset
+    dataset = pd.read_csv(data_dir, delimiter='\t', names=["documentID", "text", "entity1", "beg1", "ent1_end", "entity2", "ent2_start", "ent2_end", "label"])
+    label = []
+    for i in dataset['label']:
+        if i == 'blind':
+            label.append(100)
+        else:
+            label.append(label_type[i])
+    dataset['label'] = label
+    return dataset
+
+
+def load_pd_data_2(data_dir):
+    """
+    학습 데이터를 pandas DataFrmae 형태로 읽어들인다. 이미 label -> id 처리와 column name이 지정된 것을
+    읽어올 때 사용하는 함수이다
+    - data_dir: train tsv 파일의 경로
+    """
+    dataset = pd.read_csv(data_dir, delimiter='\t')
+    dataset["label"].value_counts()
+    return dataset
 
 
 def mask_special_token(text, start1, end1, start2, end2):
@@ -134,17 +172,21 @@ def mask_special_token(text, start1, end1, start2, end2):
     return text, new1, new2
 
 
-
+# TODO: 이거 원래대로 돌려놓기
 # Token화 된 전후로 [REL] 토큰의 위치를 잘 찾고 있는지 확인하기 위한 함수. load_data.ipynb 확인
 # 일단 [REL] 이 추가되었고 토큰 id가 35000이 되므로 char_to_token으로 비교한 것이 잘 되는지 확인함
-def tokenize_check(sample_result, tokenizer, check_token_id=35000):
+def tokenize_check(sample_result, ent1_locs, ent2_locs, tokenizer, check_token_id=35000):
     """
     Special token을 추가하고 만든 문장에서, 해당 special token의 위치를 잘 잡았는지 확인하기 위함
     0 - text ([REL] 토큰 추가된 텍스트)
     1 - 첫째 REL 토큰의 원본텍스트에서의 시작
     2 - 둘째 REL 토큰의 원본텍스트에서의 시작
     """
-    tokenized_sentence = tokenizer(sample_result[0]).input_ids
-    tokenized_index_1 = tokenizer(sample_result[0]).char_to_token(sample_result[1])
-    tokenized_index_2 = tokenizer(sample_result[0]).char_to_token(sample_result[2])
-    return (tokenized_sentence[tokenized_index_1] == check_token_id) and (tokenized_sentence[tokenized_index_2] == check_token_id)
+    for i in range(len(ent1_locs)):
+        tokenized_sentence = sample_result.input_ids[i]
+        tokenized_index_1 = sample_result.char_to_token(i, ent1_locs[i])
+        tokenized_index_2 = sample_result.char_to_token(i, ent2_locs[i])
+        print(tokenized_index_1, tokenized_sentence[tokenized_index_1])
+        print(tokenized_index_2, tokenized_sentence[tokenized_index_2])
+        # print(tokenizer.decode(tokenized_sentence), tokenized_index_1, tokenized_index_2, tokenized_sentence[tokenized_index_1], tokenized_sentence[tokenized_index_2])
+        assert (tokenized_sentence[tokenized_index_1] == check_token_id) and (tokenized_sentence[tokenized_index_2] == check_token_id)
